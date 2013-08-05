e2c72a8432ce52af7db32a07de5e693985291461
= <rjose@ejorp.com>
2013-08-05 12:18:49 -0700

    Add reference to WebUI
    - Need a global reference for qplan server

--- server/app/qplan.lua
+++ server/app/qplan.lua
@@ -1,5 +1,8 @@
 package.path = package.path .. ";app/?.lua;modules/?.lua"
 
+-- NOTE: This is a global object that we refer to in the qplan server
+WebUI = require('app/web_ui')
+
 function qplan_init(version)
         -- Do any init stuff here
 end
