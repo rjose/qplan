87cb1b491fba3452ce2f4d6ecfe74156d733f8b7
= <rjose@ejorp.com>
2013-08-05 20:48:12 -0700

    Update shell script to have options
    - The -m option will print # people instead of man-weeks

--- supply_sum.sh
+++ supply_sum.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/zsh
 
-quarter=$1
-year=$2
 
-weeks_left=$(weeks_left.py $quarter $year)
+while getopts ":mh:" option; do
+        case "$option" in 
+                (m)
+                        weeks_left=1
+                        ;;
 
+                (h) 
+                        echo "Usage: $0 [-m] quarter [year]" >&2;
+                        echo "  -m: print total people for each skill instead of man-weeks" >&2;
+                        exit 1;
+                        ;;
+
+        esac
+done
+shift $(( OPTIND - 1))
+
+# Figure out weeks left
+if [[ -z $weeks_left ]]; then
+        quarter=$1
+        year=$2
+        weeks_left=$(weeks_left.py $quarter $year)
+fi
+
+# Sum people skills (assuming skills are in col 3)
 prog='
 BEGIN {
         FS = "\t"
