93976c356c4ceeb1684568bb36dd5831ecb35fec
= <rjose@ejorp.com>
2013-08-19 15:29:12 -0700

    Add x and y scale

--- server/public/js/charts.js
+++ server/public/js/charts.js
@@ -4,9 +4,6 @@ var width = 500;
 var height = 500;
 
 function QuadrantDemoCtrl($scope, $element) {
-        console.log("Howdy");
-        console.log($element[0]);
-
         var svg = d3.select($element[0])
                 .append("svg")
                 .attr("width", width)
@@ -20,12 +17,22 @@ function QuadrantDemoCtrl($scope, $element) {
                 [100, 30]
         ];
 
+        var maxEffort = d3.max(dataset, function(d) {return d[0]});
+        var maxValue = d3.max(dataset, function(d) {return d[1]});
+
+        var xScale = d3.scale.linear()
+                .domain([0, maxEffort])
+                .range([0, width]);
+        var yScale = d3.scale.linear()
+                .domain([0, maxValue])
+                .range([0, height]);
+
         svg.selectAll("circle")
                 .data(dataset)
                 .enter()
                 .append("circle")
-                .attr("cx", function(d) {return d[0];})
-                .attr("cy", function(d) {return d[1];})
+                .attr("cx", function(d) {return xScale(d[0])})
+                .attr("cy", function(d) {return yScale(d[1])})
                 .attr("r", 5);
 }
 
