6a4922fac6fe04361e11c8ba5879ee5e3c4cceff
= <rjose@ejorp.com>
2013-08-19 15:49:37 -0700

    Add quadrants and margins

--- server/public/js/charts.js
+++ server/public/js/charts.js
@@ -9,6 +9,8 @@ function QuadrantDemoCtrl($scope, $element) {
                 .attr("width", width)
                 .attr("height", height);
 
+        var margin = 10;
+
         // This dataset will have been sent by the client.
         var dataset = [
                 [5, 20],
@@ -22,11 +24,38 @@ function QuadrantDemoCtrl($scope, $element) {
 
         var xScale = d3.scale.linear()
                 .domain([0, maxEffort])
-                .range([0, width]);
+                .range([margin, width - margin]);
         var yScale = d3.scale.linear()
                 .domain([0, maxValue])
-                .range([0, height]);
+                .range([margin, height - margin]);
 
+        // Add quadrants
+        var vertLine = svg.append("line")
+                .attr("x1", height/2)
+                .attr("y1", 0)
+                .attr("x2", height/2)
+                .attr("y2", width)
+                .style("stroke-width", 2)
+                .style("stroke", "grey");
+
+        var horizLine = svg.append("line")
+                .attr("x1", 0)
+                .attr("y1", height/2)
+                .attr("x2", width)
+                .attr("y2", height/2)
+                .style("stroke-width", 2)
+                .style("stroke", "grey");
+
+        var frame = svg.append("rect")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("width", width)
+                .attr("height", height)
+                .style("fill", "none")
+                .style("stroke-width", 3)
+                .style("stroke", "black");
+
+        // Add circles
         svg.selectAll("circle")
                 .data(dataset)
                 .enter()
