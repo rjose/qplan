106a627f7452baa25ba77417dd28aaf42ccec35a
= <rjose@ejorp.com>
2013-08-23 13:16:02 -0700

    Add commandline flag to bcast for broadcast type
            - This is in anticipation of moving all broadcast logic to python
              modules and running everything through "bcast"
            - Remove broadcast_piechart
            - Ignore .pyc files

---
+++ python_modules/bcast/piechart.py
@@ -0,0 +1,29 @@
+import json
+
+#-------------------------------------------------------------------------------
+# Formats text from file "f" as JSON piechart data.
+#
+# The first line of the text is taken to be the title of the chart. The
+# following lines should consist of two fields separated by a tab. The first
+# field is the label of the field; the second is the value. For example:
+#
+#       This is the title of the chart
+#       Label1\t10
+#       Label2\t20
+#  
+def format_text(f):
+        title = f.readline()
+
+        dataset = []
+        for line in f.readlines():
+                fields = line.split("\t")
+                dataset.append({'value': fields[1], 'label': fields[0]})
+
+        result = {'command': 'chart',
+                  'title': title,
+                  'data' : {
+                    'type': 'piechart',
+                    'dataset': dataset
+                  }
+                 }
+        return json.dumps(result)
