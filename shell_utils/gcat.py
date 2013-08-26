f2872dc3593f57f548bbfac075594935b7f1600a
Rino Jose <rjose@linkedin.com>
2013-08-26 13:46:39 -0700

    Change to grab all columns from worksheet (again)

--- shell_utils/gcat.py
+++ shell_utils/gcat.py
@@ -25,28 +25,11 @@ def removeNonAscii(s):
 def cat_tables(section):
         print "=====%s" % section
         for p in source_info.items(section):
-                [spreadsheet_key, worksheet_index, col_str] = p[1].split(":")
+                [spreadsheet_key, worksheet_index] = p[1].split(":")
                 spreadsheet = gc.open_by_key(spreadsheet_key)
                 worksheet = spreadsheet.get_worksheet(int(worksheet_index))
-                col_ids = col_str.split(",")
 
-                cols = [worksheet.col_values(int(i)) for i in col_ids]
-                if len(cols) == 0:
-                        return
-                list_of_lists = []
-                max_len = max([len(c) for c in cols])
-                for r in range(max_len):
-                        row = []
-                        for c in range(len(cols)):
-                                item = ''
-                                if r < len(cols[c]):
-                                        item = cols[c][r]
-                                if item is None:
-                                        item = ''
-                                row.append(item)
-                        list_of_lists.append(row)
-
-                #list_of_lists = worksheet.get_all_values()
+                list_of_lists = worksheet.get_all_values()
                 for row in list_of_lists:
                         print removeNonAscii('\t'.join(row))
         return
