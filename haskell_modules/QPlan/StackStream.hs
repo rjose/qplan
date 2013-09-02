3040d4e1148a710033ffd7d483ae34ddef9dfc11
= <rjose@ejorp.com>
2013-09-02 09:27:28 -0700

    Add unit test for unstack

--- haskell_modules/QPlan/StackStream.hs
+++ haskell_modules/QPlan/StackStream.hs
@@ -1,5 +1,6 @@
 module StackStream (
-        unstack
+        unstack,
+        streamHeader
 ) where
 
 import Data.List
@@ -16,12 +17,15 @@ data Stream
         | EmptyStream
         deriving (Show)
 
--- TODO: Look into using the State monad when unstacking the stream
 fromString :: String -> Maybe Header
 fromString s
         | "=====" `isPrefixOf` s = Just $ Header $ drop prefixLen s
         | otherwise = Nothing
                 where prefixLen = 5
+
+streamHeader :: Stream -> String
+streamHeader (Stream (Header h) _) = h
+streamHeader _ = ""
 
 nextState :: State [String] Stream
 nextState = state $ getStream
@@ -39,38 +43,3 @@ unstack [] = []
 unstack ss = stream:unstack rest
         where
                 (stream, rest) = getStream ss
-
-
--- DATA
-
-input="=====title\n" ++
-      "\tShortage Chart (with shortage)\n" ++
-      "=====demand\n" ++
-      "\tTrack 1\t20\n" ++
-      "\tTrack 2\t35\n" ++
-      "=====shortage\n" ++
-      "\tApps\t5\n" ++
-      "\tNative\t3\n" ++
-      "\tQA\t1"
-
-linput = lines input
---unstack :: String -> [Stream]
---unstack input = unlines shortage_data
---        where
---                streams = getStreams . lines $ input
---                (Stream _ shortage_data) = streams !! 2
---
---
---getStream :: [String] -> (Stream, [String])
---getStream (s:ss) = (Stream (fromJust $ getField s) $ stream_lines, snd parts)
---        where
---                parts = break (isJust . getField) ss
---                -- Remove leading tab from stream data
---                stream_lines = map tail $ fst parts 
---
---getStreams :: [String] -> [Stream]
---getStreams [] = []
---getStreams ss = stream:rest
---        where
---                (stream, rest_strs) = getStream ss
---                rest = getStreams rest_strs
