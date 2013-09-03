cf2669993df1a7403fd5948e9816b2dacfbb010f
Rino Jose <rjose@linkedin.com>
2013-09-03 10:53:26 -0700

    Remove server files
            - NOTE: These have been moved to catserve

--- server/tcp.h
+++
@@ -1,8 +0,0 @@
-#ifndef TCP_H
-#define TCP_H
-
-ssize_t my_buffered_read(int fd, char *ptr, size_t maxlen);
-ssize_t my_readline(int fd, void *vptr, size_t maxlen);
-int my_writen(int fd, const void *ptr, size_t nbytes);
-
-#endif
