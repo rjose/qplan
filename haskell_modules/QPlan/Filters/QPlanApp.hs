6a584dcaccd714c8ee56d541651ca7c3126e470d
= <rjose@ejorp.com>
2013-09-07 18:22:31 -0700

    Add comments to QPlanApp

--- haskell_modules/QPlan/Filters/QPlanApp.hs
+++ haskell_modules/QPlan/Filters/QPlanApp.hs
@@ -1,3 +1,17 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Filters.QPlanApp
+-- Copyright   :  (c) Rino Jose 2013
+-- License     :  BSD-style
+--
+-- Maintainer  :  @rjose
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Converts work and staff streams into JSON data appropriate for QPlan app.
+-- This enables analysis of staff skill shortages by triage.
+--
+-----------------------------------------------------------------------------
 module Filters.QPlanApp (filterString) where
 
 import Data.List
@@ -11,6 +25,9 @@ import Work
 import Person
 import SkillAmount
 
+-- =============================================================================
+-- Data types
+--
 type SkillName = String
 type TrackName = String
 type TrackWork =  [[Work]] -- List of tracks, each with a list of work
@@ -20,6 +37,17 @@ type TrackDemand = [[[Float]]] -- Tracks, each with a triage list, each with ski
 type TrackAvail = TrackDemand -- Same size and shape as TrackDemand
 type TrackFeasibility = [[Bool]] -- List of tracks, each with bool list corresp. to worklist
 
+
+-- =============================================================================
+-- Public API
+--
+
+--------------------------------------------------------------------------------
+-- | Converts work and staff stacked streams into JSON data for QPlan.
+--
+--      This precomputes all combinations of track and triage requests that
+--      might come from a QPlan user.
+--
 filterString :: String -> String
 filterString s = if any isNothing [workStream, staffStream]
                         then ""
@@ -54,7 +82,16 @@ filterString s = if any isNothing [workStream, staffStream]
                   ("track_work", trackWorkToJSValue trackWork trackFeasibility) ]
 
 
+-- =============================================================================
+-- Internal functions
+--
 
+--------------------------------------------------------------------------------
+-- Takes union of tracks from a list of people and work items.
+--
+--      NOTE: This also prepends the "All" track to represent items from all
+--      tracks.
+--
 getTracks :: [Person] -> [Work] -> [TrackName]
 getTracks staff workItems = result
         where
@@ -63,6 +100,9 @@ getTracks staff workItems = result
                 result' = Set.toList $ Set.union workTracks staffTracks
                 result = "All":(sort result')
 
+--------------------------------------------------------------------------------
+-- Takes union of skills from a list of people and work items.
+--
 getSkills :: [Person] -> [Work] -> [SkillName]
 getSkills staff workItems = result
         where
@@ -73,13 +113,26 @@ getSkills staff workItems = result
                 result = sort $ filter (\s -> s /= "") result'
 
 
+--------------------------------------------------------------------------------
+-- Groups work items by specified track names.
+--
+--      NOTE: All of the work items "ws" are prepended to the result. This
+--      corresponds to the "All" track.
+--
 workByTrack :: [TrackName] -> [Work] -> TrackWork
 workByTrack (all:tracks) ws = result
         where
-                result = map (sortBy (\l r -> rank l `compare` rank r)) $ ws:result'
                 result' = (\t -> filter (\w -> t == Work.track w) ws) <$> tracks
+                result = map (sortBy (\l r -> rank l `compare` rank r)) $ ws:result'
 
 
+--------------------------------------------------------------------------------
+-- Organizes staff by track and then by skill within each track.
+--
+--      The track names and skills in question are provided as arguments. In
+--      effect, this allows us to assume the order of items in lists is
+--      meaningful (e.g., the first item here corresponds to the first track).
+--
 staffByTrackSkills :: [TrackName] -> [SkillName] -> [Person] -> TrackStaff
 staffByTrackSkills (all:tracks) skills staff = result
         where
@@ -89,12 +142,30 @@ staffByTrackSkills (all:tracks) skills staff = result
           result = map groupBySkills staffByTrack
 
 
+--------------------------------------------------------------------------------
+-- Computes manpower for track staff.
+--
+--      This also uses a "scale" function to provide info on resource
+--      availability. For instance, this would typically map people to 13 (for
+--      13 weeks in a quarter).
+--
 getManpower :: (Person -> Float) -> TrackStaff -> TrackManpower
 getManpower scale staff = result
         where
                 result = [trackPower | trackStaff <- staff,
                           let trackPower = map (foldr (\p a -> a + scale p) 0) trackStaff]
 
+
+--------------------------------------------------------------------------------
+-- Computes manpower requirements for track work items.
+--
+--      Track work is divided into triage groups. The demand is reported as a
+--      running total across the triage groups, from high to low priority.
+--
+--      The resulting manpower skill requirements will be returned in the same
+--      order as the "skills" list. If a skill is not required for a work item,
+--      0 will be returned as the skill requirement.
+--
 getTrackDemand :: TrackWork -> [SkillName] -> TrackDemand
 getTrackDemand trackWork skills = result
         where
@@ -111,6 +182,9 @@ getTrackDemand trackWork skills = result
                 len = length skills
 
 
+--------------------------------------------------------------------------------
+-- Maps an empty demand array into one with a specified number of zeroes.
+--
 conditionDemand :: Int -> [Float] -> [Float]
 conditionDemand len demand = result
         where
@@ -118,10 +192,20 @@ conditionDemand len demand = result
                          then take len $ repeat 0
                          else demand
 
+--------------------------------------------------------------------------------
+-- Accumulates manpower totals.
+--
 accManpower :: [[Float]] -> [[Float]]
 accManpower [] = []
 accManpower (m:ms) = scanl (\acc mp -> sumManpower [acc, mp]) m ms
 
+
+--------------------------------------------------------------------------------
+-- Sums list of manpower items.
+--
+--      NOTE: The position in the manpower arrays corresponds to whatever skills
+--      array was used in the calling functions.
+--
 sumManpower :: [[Float]] -> [Float]
 sumManpower [] = []
 sumManpower (m:ms) = result
@@ -129,11 +213,26 @@ sumManpower (m:ms) = result
                 result = foldl (\acc mp -> zipWith (+) acc mp) m ms
 
 
+--------------------------------------------------------------------------------
+-- Computes net available manpower.
+--
+--      This takes manpower for each track along with the running skill demands
+--      for each track by triage and computes the net availability for each
+--      triage group in each track.
+--
 getNetAvail :: TrackManpower -> TrackDemand -> TrackAvail
 getNetAvail manpower demand = result
         where
                 result = zipWith (\mp d -> map (zipWith (-) mp) d) manpower demand
 
+
+--------------------------------------------------------------------------------
+-- Computes feasibility of track work.
+--
+--      This checks the net availability across all items in all tracks and
+--      returns corresponding Bool values if any of the required skills for an
+--      item have been exhausted.
+--
 getTrackFeasibility :: [SkillName] -> TrackManpower -> TrackWork -> TrackFeasibility
 getTrackFeasibility skills manpower trackWork = result
         where
@@ -145,8 +244,13 @@ getTrackFeasibility skills manpower trackWork = result
                 trackAvail = zipWith netAvail manpower trackDemand
                 result = zipWith (zipWith isFeasibile) trackAvail trackDemand
 
+-- =============================================================================
+-- JSON functions
+--
 
-
+--------------------------------------------------------------------------------
+-- Constructs a JSValue for track stats.
+--
 trackStatsToJSValue :: TrackManpower -> TrackDemand -> TrackAvail -> JSValue
 trackStatsToJSValue manpower demand avail = result
         where
@@ -157,24 +261,50 @@ trackStatsToJSValue manpower demand avail = result
                 demand' = JSArray [JSArray $ map floatsToJSValue d | d <- demand]
                 netAvail' = JSArray [JSArray $ map floatsToJSValue a | a <- avail]
 
+
+--------------------------------------------------------------------------------
+-- Constructs a JSValue for track staff.
+--
 trackStaffToJSValue :: TrackStaff -> JSValue
 trackStaffToJSValue staff = JSArray [JSArray $ map peopleToJSValue ss | ss <- staff]
 
+--------------------------------------------------------------------------------
+-- Constructs a JSValue for track work.
+--
 trackWorkToJSValue :: TrackWork -> TrackFeasibility -> JSValue
 trackWorkToJSValue work feasibilty = JSArray $ zipWith workListToJSValue work feasibilty
 
+--------------------------------------------------------------------------------
+-- Converts list of String to JSValue.
+--
 stringsToJSValue :: [String] -> JSValue
 stringsToJSValue strings = JSArray $ map (JSString . toJSString) strings
 
+--------------------------------------------------------------------------------
+-- Converts list of Floats to JSValue.
+--
 floatsToJSValue :: [Float] -> JSValue
 floatsToJSValue floats = JSArray $ map (JSRational False . toRational) floats
 
+--------------------------------------------------------------------------------
+-- Converts list of people to a JSValue.
+--
 peopleToJSValue :: [Person] -> JSValue
 peopleToJSValue people = JSArray $ map personToJSValue people
 
+--------------------------------------------------------------------------------
+-- Converts list of work items to JSValue.
+--
+--      NOTE: This takes a list of Bool indicating the feasibility of each item.
+--
 workListToJSValue :: [Work] -> [Bool] -> JSValue
 workListToJSValue ws fs = JSArray $ zipWith workToJSValue ws fs
 
+--------------------------------------------------------------------------------
+-- Converts individual work item to a JSValue.
+--
+--      NOTE: This also takes a bool indicating if the work item is feasible.
+--
 workToJSValue :: Work -> Bool -> JSValue
 workToJSValue w isFeasible = makeObj [
         ("estimate", JSString $ toJSString $ format $ estimate w),
@@ -186,6 +316,9 @@ workToJSValue w isFeasible = makeObj [
         where
                 format estimates = intercalate ", " $ map show estimates
 
+--------------------------------------------------------------------------------
+-- Converts person to a JSValue.
+--
 personToJSValue :: Person -> JSValue
 personToJSValue p = makeObj [
         ("name", JSString $ toJSString $ Person.name p) ]
