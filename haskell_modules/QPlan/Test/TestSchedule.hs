d6ee7d911cbe49e299bd175d25326cb17591bf54
= <rjose@ejorp.com>
2013-09-11 20:10:24 -0700

    Add more scheduling tests

--- haskell_modules/QPlan/Test/TestSchedule.hs
+++ haskell_modules/QPlan/Test/TestSchedule.hs
@@ -12,32 +12,49 @@ import Filters.Utils
 import Work
 
 
-
-
-
-
 testScheduleWork :: Assertion
 testScheduleWork = do
-        assertEqual "" [Just date1, Just date2] result
+        assertEqual "No work" [] (schedule [] schedAvail)
+        assertEqual "1 item, 1 skill" [Just oct4] (schedule [w1] schedAvail)
+        assertEqual "2 items, 1 skill" [Just oct4, Just oct9] (schedule ws1 schedAvail)
+        assertEqual "2 skills" [Just oct11] (schedule ws2 schedAvail)
+        assertEqual "2 skill, then 1" [Just oct11, Just oct9] (schedule ws3 schedAvail)
+        assertEqual "5 in parallel" [Just oct10] (schedule [w3] schedAvail)
+        assertEqual "Push w3 out" [Just oct11, Just oct18] (schedule [w2, w3] schedAvail)
         where
                 startDate = stringToDay "Sep 29, 2013"
                 endDate = stringToDay "Oct 31, 2013"
-                date1 = stringToDay "Oct 4, 2013"
-                date2 = stringToDay "Oct 9, 2013"
+                oct4 = stringToDay "Oct 4, 2013"
+                oct9 = stringToDay "Oct 9, 2013"
+                oct10 = stringToDay "Oct 10, 2013"
+                oct11 = stringToDay "Oct 11, 2013"
+                oct18 = stringToDay "Oct 18, 2013"
 
-                workline1 = "ABC123\t30\tAn item of work\tApps:S\t1.5\tTrack1\tMobile\t8\tB21,C23"
-                w1 = workFromString workline1
-                work = [w1, w1]
+                w1 = workFromString "1\t3\tW1\tApps:S\t1\tTrack1\tMobile\t8\tB21"
+                w2 = workFromString "2\t3\tW1\tApps:S,Web:M\t1\tTrack1\tMobile\t8\tB21"
+                w3 = workFromString "3\t3\tW1\tNative:5S,Web:S\t1\tTrack1\tMobile\t8\tB21"
+                ws1 = [w1, w1]
+                ws2 = [w2]
+                ws3 = [w2, w1]
 
                 skills = ["Apps", "Native", "Web"]
                 days = getDays startDate endDate
-                workweek = [0, 2, 2, 1, 1, 1, 0]
-                schedAvail = (days, [take (length days) $ cycle workweek])
+                appsWorkweek = [0, 2, 2, 1, 1, 1, 0]
+                nativeWorkweek = [0, 3, 3, 3, 3, 3, 0]
+                webWorkweek = [0, 1, 1, 1, 1, 1, 0]
+                schedAvail = (days, [
+                                    take (length days) $ cycle appsWorkweek,
+                                    take (length days) $ cycle nativeWorkweek,
+                                    take (length days) $ cycle webWorkweek
+                                    ])
 
-                schedule = do
-                        ds <- mapM (state . scheduleWork skills) work
-                        return ds
-                (result, _) = runState schedule schedAvail
+                schedule work avail = result'
+                        where
+                                (result', _) = runState (schedule' work) avail
+                                schedule' work' = do
+                                        ds <- mapM (state . scheduleWork skills) work'
+                                        return ds
+
 
 
 testConsumeResource :: Assertion
