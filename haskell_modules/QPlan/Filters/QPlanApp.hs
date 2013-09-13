90ac32e46a9dbef650d75974e4c177415b9c47a6
= <rjose@ejorp.com>
2013-09-12 21:27:29 -0700

    Tighten up code
            - Remove unused JSON functions

--- haskell_modules/QPlan/Filters/QPlanApp.hs
+++ haskell_modules/QPlan/Filters/QPlanApp.hs
@@ -22,7 +22,6 @@ import Data.Time.Calendar
 import Data.Time.Format
 import qualified Data.Set as Set
 import System.Locale
-import Text.JSON
 
 import Filters.Schedule.Internal
 import Filters.Utils
@@ -49,10 +48,7 @@ type TrackFeasibility = [[Bool]] -- List of tracks, each with bool list corresp.
 --
 
 --------------------------------------------------------------------------------
--- | Converts work and staff stacked streams into JSON data for QPlan.
---
---      This precomputes all combinations of track and triage requests that
---      might come from a QPlan user.
+-- | Converts qplan input streams into result streams for qplan.
 --
 filterString :: String -> String
 filterString s = if any isNothing [workStream, staffStream, holidayStream,
@@ -75,7 +71,6 @@ filterString s = if any isNothing [workStream, staffStream, holidayStream,
                 staff = sort $ map personFromString $ content $ fromJust staffStream
 
                 -- Define "master lists"
-                --
                 --      Any list organized by track, skill, or time will
                 --      correspond directly to these lists.
                 tracks = getTracks staff workItems
@@ -90,9 +85,9 @@ filterString s = if any isNothing [workStream, staffStream, holidayStream,
                 -- Compute resource demand and feasibility
                 trackManpower = getManpower (\p -> numWeeks) trackStaff
                 trackDemand = getTrackDemand trackWork skills
-                trackAvail = getNetAvail trackManpower trackDemand
                 trackFeasibility = getTrackFeasibility skills trackManpower trackWork
 
+                -- TODO: Tighten the rest of this code up
                 -- Schedule work based on track assignments
                 schedSkills = splitOn ":" schedSkillStr
                 workDays = map (isWorkDay holidays) days
@@ -100,6 +95,7 @@ filterString s = if any isNothing [workStream, staffStream, holidayStream,
                 trackDates = getTrackWorkDates schedSkills trackWork days trackStaffAvail
 
                 -- Generate result
+                -- TODO: Have StacKStream do the stream construction for us
                 trackStream = ["=====qplan tracks v1"] ++ (map addTab tracks)
                 skillStream = ["=====qplan skills v1"] ++ (map addTab skills)
                 triageStream = ["=====qplan triages v1"] ++ (map addTab triages)
@@ -123,7 +119,6 @@ filterString s = if any isNothing [workStream, staffStream, holidayStream,
                                            trackStream, skillStream, triageStream,
                                            manpowerStream, trackDemandStream,
                                            trackStaffStream, trackWorkStream]
-
 
 
 -- =============================================================================
@@ -169,45 +164,24 @@ getTriageStream manpower = result
                                 let l = joinWith "\t" $ map show mp]
 
 
+-- TODO: Move some of these functions to Utils
 joinWith :: String -> [String] -> String
 joinWith _ [] = []
 joinWith _ [w] = w
 joinWith c (w:ws) = w ++ c ++ (joinWith c ws)
 
 
--- =====qplan track-triage demand v1
---      =====qplan triage v1
---              1 2 1 1
---              2 2 2 2
---              4 3 2 3
---              4 3 2 3
---      =====qplan triage v1
---              1 2 1 1
---              2 2 2 2
---              4 3 2 3
---              4 3 2 3
---
-
-
-
-
 addTab :: String -> String
 addTab s = "\t" ++ s
 
 getParams :: Stream -> (Day, Day, Float, String)
-getParams (Stream _ ls) = result
+getParams (Stream _ ls) = (startDate, endDate, (fromInteger $ round numWeeks), schedStr)
         where
                 params = splitOn "\t" (head ls)
                 startDate = stringToDay $ params !! 0
                 endDate = stringToDay $ params !! 1
                 schedStr = params !! 2
-                numDays = fromInteger ((diffDays endDate startDate) + 1) :: Float
-                numWeeks = numDays / 7.0
-                result = (startDate,
-                          endDate,
-                          (fromInteger $ round numWeeks) :: Float,
-                          schedStr
-                         )
+                numWeeks = (/ 7.0) $ fromInteger ((diffDays endDate startDate) + 1) :: Float
 
 
 getTrackWorkDates :: [SkillName] -> TrackWork -> [Day] -> [SkillAvailabilities] ->
@@ -367,19 +341,6 @@ sumManpower (m:ms) = result
 
 
 --------------------------------------------------------------------------------
--- Computes net available manpower.
---
---      This takes manpower for each track along with the running skill demands
---      for each track by triage and computes the net availability for each
---      triage group in each track.
---
-getNetAvail :: TrackManpower -> TrackDemand -> TrackAvail
-getNetAvail manpower demand = result
-        where
-                result = zipWith (\mp d -> map (zipWith (-) mp) d) manpower demand
-
-
---------------------------------------------------------------------------------
 -- Computes feasibility of track work.
 --
 --      This checks the net availability across all items in all tracks and
@@ -395,86 +356,6 @@ getTrackFeasibility skills manpower trackWork = result
                 trackDemand = [map (getWorkManpower skills) ws| ws <- trackWork]
                 trackAvail = zipWith netAvail manpower trackDemand
                 result = zipWith (zipWith isFeasibile) trackAvail trackDemand
-
--- =============================================================================
--- JSON functions
---
-
---------------------------------------------------------------------------------
--- Constructs a JSValue for track stats.
---
-trackStatsToJSValue :: TrackManpower -> TrackDemand -> TrackAvail -> JSValue
-trackStatsToJSValue manpower demand avail = result
-        where
-                result = makeObj [("manpower", manpower'),
-                                  ("demand", demand'),
-                                  ("net_avail", netAvail')]
-                manpower' = JSArray [floatsToJSValue mp | mp <- manpower]
-                demand' = JSArray [JSArray $ map floatsToJSValue d | d <- demand]
-                netAvail' = JSArray [JSArray $ map floatsToJSValue a | a <- avail]
-
-
---------------------------------------------------------------------------------
--- Constructs a JSValue for track staff.
---
-trackStaffToJSValue :: TrackStaff -> JSValue
-trackStaffToJSValue staff = JSArray [JSArray $ map peopleToJSValue ss | ss <- staff]
-
---------------------------------------------------------------------------------
--- Constructs a JSValue for track work.
---
-trackWorkToJSValue :: TrackWork -> TrackFeasibility -> JSValue
-trackWorkToJSValue work feasibilty = JSArray $ zipWith workListToJSValue work feasibilty
-
---------------------------------------------------------------------------------
--- Converts list of String to JSValue.
---
-stringsToJSValue :: [String] -> JSValue
-stringsToJSValue strings = JSArray $ map (JSString . toJSString) strings
-
---------------------------------------------------------------------------------
--- Converts list of Floats to JSValue.
---
-floatsToJSValue :: [Float] -> JSValue
-floatsToJSValue floats = JSArray $ map (JSRational False . toRational) floats
-
---------------------------------------------------------------------------------
--- Converts list of people to a JSValue.
---
-peopleToJSValue :: [Person] -> JSValue
-peopleToJSValue people = JSArray $ map personToJSValue people
-
---------------------------------------------------------------------------------
--- Converts list of work items to JSValue.
---
---      NOTE: This takes a list of Bool indicating the feasibility of each item.
---
-workListToJSValue :: [Work] -> [Bool] -> JSValue
-workListToJSValue ws fs = JSArray $ zipWith workToJSValue ws fs
-
---------------------------------------------------------------------------------
--- Converts individual work item to a JSValue.
---
---      NOTE: This also takes a bool indicating if the work item is feasible.
---
-workToJSValue :: Work -> Bool -> JSValue
-workToJSValue w isFeasible = makeObj [
-        ("estimate", JSString $ toJSString $ format $ estimate w),
-        ("feasible", JSBool isFeasible),
-        ("name", JSString $ toJSString $ Work.name w),
-        ("rank", JSRational False $ toRational $ rank w),
-        ("track", JSString $ toJSString $ Work.track w),
-        ("triage", JSString $ toJSString $ show $ triage w) ]
-        where
-                format estimates = intercalate ", " $ map show estimates
-
---------------------------------------------------------------------------------
--- Converts person to a JSValue.
---
-personToJSValue :: Person -> JSValue
-personToJSValue p = makeObj [
-        ("name", JSString $ toJSString $ Person.name p) ]
-
 
 test = do
         content <- readFile "_qplan.txt"
