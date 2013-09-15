cd9eeab2db74113d1cdde17b77d2a5ec5c89e1c3
= <rjose@ejorp.com>
2013-09-14 22:17:22 -0700

    Add code comments

--- haskell_modules/QPlan/Filters/Utils.hs
+++ haskell_modules/QPlan/Filters/Utils.hs
@@ -1,3 +1,16 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Filters.Utils
+-- Copyright   :  (c) Rino Jose 2013
+-- License     :  BSD-style
+--
+-- Maintainer  :  @rjose
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Provides utility functions for filters.
+--
+-----------------------------------------------------------------------------
 module Filters.Utils where
 
 import Data.List
@@ -6,13 +19,20 @@ import Data.Time.Calendar
 import Data.Time.Format
 import System.Locale
 
-import Work
 
+--------------------------------------------------------------------------------
+-- Standard date format for all filters.
+--
 dateFormat :: String
 dateFormat = "%b %e, %Y"
 
+
+--------------------------------------------------------------------------------
+-- Formats day as string.
+--
 dayToString :: Day -> String
 dayToString d = formatTime defaultTimeLocale dateFormat d
+
 
 --------------------------------------------------------------------------------
 -- Converts string to day.
@@ -21,6 +41,7 @@ dayToString d = formatTime defaultTimeLocale dateFormat d
 --
 stringToDay :: String -> Day
 stringToDay s = fromJust $ parseTime defaultTimeLocale dateFormat s
+
 
 --------------------------------------------------------------------------------
 -- Gets a list of dates from a start and end date (inclusive).
@@ -31,39 +52,17 @@ getDays start end
         | otherwise = start:getDays (addDays 1 start) end
 
 
-getRequiredStaff :: [String] -> Work -> [(NumStaff, NumWeeks)]
-getRequiredStaff skills work = result
-        where
-                result = [reqStaff | s <- skills,
-                           let estimates = estimate work
-                               reqStaff' = find (\e -> s == skill' e) estimates
-                               reqStaff = if isNothing reqStaff'
-                                          then (0, 0)
-                                          else requiredStaff $ fromJust reqStaff'
-                         ]
-
 --------------------------------------------------------------------------------
--- | Returns required manpower in terms of specified skills.
+-- Joins Strings together.
 --
---      The [String] list has a list of the skills that correspond to the
---      returned list of Floats.
---
-getWorkManpower :: [String] -> Work -> [Float]
-getWorkManpower skills work = result
-        where
-                result = [manpower | s <- skills,
-                           let estimates = estimate work
-                               manpower' = find (\e -> s == skill' e) estimates
-                               manpower = if isNothing manpower'
-                                          then 0
-                                          else numval' $ fromJust manpower'
-                         ]
-
 joinWith :: String -> [String] -> String
 joinWith _ [] = []
 joinWith _ [w] = w
 joinWith c (w:ws) = w ++ c ++ (joinWith c ws)
 
 
+--------------------------------------------------------------------------------
+-- Adds tab to beginning of String.
+--
 addTab :: String -> String
 addTab s = "\t" ++ s
